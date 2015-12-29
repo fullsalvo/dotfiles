8d74abd72c7ddad37b3d38ab4c2c202d628e54b9
Dimitri Baptiste <escambiadrummer@gmail.com>
2015-12-29 02:00:28 -0600

    fullsalvo update

--- .bin/signal.sh
+++ .bin/signal.sh
@@ -6,7 +6,10 @@ network=$(iwgetid -r)
 if [[ $network == "" ]]; then
     exit 0
 else
-    signal=$(nmcli dev wifi | grep $network)
+    #signal=$(nmcli dev wifi | grep $network)
+    signal=$(cat /proc/net/wireless | awk 'NR==3' | awk '{ print $3 }' | tr -d '.')
+#    percentage=$(bc $signal / 70 * 100 | awk '{ print $1 }' | tr -d '~')
+#    whole=$(echo $percentage | grep -Eo '[0-9]{1,3}' | awk NR==1)
 fi
 
-echo $signal
+echo $signal/70
