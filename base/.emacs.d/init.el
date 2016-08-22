062cbe02d5455dd64ed6a3a91c2461a7d0b574d2
fullsalvo <escambiadrummer@gmail.com>
2016-08-22 07:11:09 -0400

    added bar starting script to lemonbar folder, using whizkers to change it

--- base/.emacs.d/init.el
+++ base/.emacs.d/init.el
@@ -117,7 +117,7 @@
 (setq initial-scratch-message "")
 
 ;; Open to init file by default
-(setq initial-buffer-choice "~")
+;; (setq initial-buffer-choice "~")
 
 ;; Close the scratch buffer when entering a programming mode
 (add-hook 'prog-mode-hook 'remove-scratch-buffer)
