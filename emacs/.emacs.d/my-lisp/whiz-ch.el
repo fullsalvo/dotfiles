17fb984aef17a2467642da481a8d8af84f77a133
fullsalvo <escambiadrummer@gmail.com>
2017-11-15 12:48:34 -0500

    wires theme

--- emacs/.emacs.d/my-lisp/whiz-ch.el
+++ emacs/.emacs.d/my-lisp/whiz-ch.el
@@ -1,3 +1,3 @@
 ;; use custom font faces
-(set-default-font "Office Code Pro 9")
-(set-fontset-font "fontset-default" 'unicode '("Office Code Pro 9" . "UTF-8"))
+(set-default-font "Fira Code 9")
+(set-fontset-font "fontset-default" 'unicode '("Fira Code 9" . "UTF-8"))
